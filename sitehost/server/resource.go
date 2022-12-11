3d341f7cbb63a4e0eb75c364a0bd54b66781902a
Tommy <61849757+tommysitehost@users.noreply.github.com>
2022-12-12 10:14:05 +1300

    Add CHANGELOG and update GoSH version (#7)
    * Add CHANGELOG and update GoSH version
    
    * Add control Mysth response in update and upgrade action
    
    * Update syntax
    
    * Update GoSH v0.3.0 and fix linter errors
    
    * Update CHANGELOG
    
    Co-authored-by: Gonzalo Rios <gonzalo@sitehost.co.nz>

--- sitehost/server/resource.go
+++ sitehost/server/resource.go
@@ -8,11 +8,12 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/sitehostnz/gosh"
+	"github.com/sitehostnz/gosh/pkg/api/server"
+	"github.com/sitehostnz/gosh/pkg/models"
 	"github.com/sitehostnz/terraform-provider-sitehost/sitehost/helper"
 )
 
-// Resource returns a schema with the operations for Server resource.
+// Resource returns a schema with the operations for server.
 func Resource() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: createResource,
@@ -24,14 +25,15 @@ func Resource() *schema.Resource {
 	}
 }
 
-// createResource is a function to create a new Server resource.
+// createResource is a function to create a new server.
 func createResource(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
 	conf, ok := meta.(*helper.CombinedConfig)
 	if !ok {
 		return diag.Errorf("failed to convert meta object")
 	}
 
-	client := conf.Client
+	client := server.New(conf.Client)
+
 	keys, ok := d.Get("ssh_keys").([]any)
 	if !ok {
 		return diag.Errorf("failed to convert ssh keys object")
@@ -42,37 +44,41 @@ func createResource(ctx context.Context, d *schema.ResourceData, meta any) diag.
 		sshKeys = append(sshKeys, fmt.Sprint(key))
 	}
 
-	opts := &gosh.ServerCreateRequest{
+	opts := server.CreateRequest{
 		Label:       fmt.Sprint(d.Get("label")),
 		Location:    fmt.Sprint(d.Get("location")),
 		ProductCode: fmt.Sprint(d.Get("product_code")),
 		Image:       fmt.Sprint(d.Get("image")),
-		Params: gosh.ParamsOptions{
+		Params: server.ParamsOptions{
 			SSHKeys: sshKeys,
 		},
 	}
 
-	res, err := client.Servers.Create(ctx, opts)
+	res, err := client.Create(ctx, opts)
 	if err != nil {
 		return diag.Errorf("Error creating server: %s", err)
 	}
 
+	if !res.Status {
+		return diag.Errorf("Error creating server: %s", res.Msg)
+	}
+
 	// Set data
 	d.SetId(res.Return.Name)
-	if err = d.Set("name", res.Return.Name); err != nil {
+	if err := d.Set("name", res.Return.Name); err != nil {
 		return diag.FromErr(err)
 	}
 
-	if err = d.Set("password", res.Return.Password); err != nil {
+	if err := d.Set("password", res.Return.Password); err != nil {
 		return diag.FromErr(err)
 	}
 
-	if err = d.Set("ips", res.Return.Ips); err != nil {
+	if err := d.Set("ips", res.Return.Ips); err != nil {
 		return diag.FromErr(err)
 	}
 
 	// wait for "Completed" status
-	if err = helper.WaitForAction(client, res.Return.JobID); err != nil {
+	if err := helper.WaitForAction(conf.Client, res.Return.JobID); err != nil {
 		return diag.FromErr(err)
 	}
 
@@ -81,86 +87,130 @@ func createResource(ctx context.Context, d *schema.ResourceData, meta any) diag.
 	return nil
 }
 
-// readResource is a function to read a new Server resource.
+// readResource is a function to read a new server.
 func readResource(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
 	conf, ok := meta.(*helper.CombinedConfig)
 	if !ok {
 		return diag.Errorf("failed to convert meta object")
 	}
 
-	client := conf.Client
+	client := server.New(conf.Client)
 
-	server, err := client.Servers.Get(context.Background(), d.Id())
+	resp, err := client.Get(context.Background(), server.GetRequest{
+		ServerName: d.Id(),
+	})
 	if err != nil {
 		return diag.Errorf("Error retrieving server: %s", err)
 	}
 
-	if err = setServerAttributes(d, server); err != nil {
+	if !resp.Status {
+		return diag.Errorf("Error retrieving server: %s", resp.Msg)
+	}
+
+	if err := setServerAttributes(d, resp.Server); err != nil {
 		return diag.FromErr(err)
 	}
 
 	return nil
 }
 
-// updateResource is a function to update a new Server resource.
+// updateResource is a function to update a server.
 func updateResource(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
 	conf, ok := meta.(*helper.CombinedConfig)
 	if !ok {
 		return diag.Errorf("failed to convert meta object")
 	}
 
-	client := conf.Client
+	client := server.New(conf.Client)
 
 	if d.HasChange("product_code") {
-		if err := client.Servers.Upgrade(context.Background(), &gosh.ServerUpgradeRequest{Name: d.Id(), Plan: fmt.Sprint(d.Get("product_code"))}); err != nil {
-			return diag.FromErr(err)
-		}
-
-		resp, err := client.Servers.CommitChanges(context.Background(), d.Id())
-		if err != nil {
-			return diag.FromErr(err)
-		}
-
-		if err = helper.WaitForAction(client, resp.Return.JobID); err != nil {
-			return diag.FromErr(err)
-		}
-
-		return nil
+		return upgradePlan(conf, client, d)
 	}
 
 	if d.HasChange("label") {
-		if err := client.Servers.Update(context.Background(), &gosh.ServerUpdateRequest{Name: d.Id(), Label: fmt.Sprint(d.Get("label"))}); err != nil {
-			return diag.FromErr(err)
-		}
-
-		return nil
+		return updateLabel(client, d)
 	}
 
 	return readResource(ctx, d, meta)
 }
 
-// deleteResource is a function to delete a new Server resource.
-func deleteResource(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
-	conf, ok := meta.(*helper.CombinedConfig)
-	if !ok {
-		return diag.Errorf("failed to convert meta object")
-	}
-
-	client := conf.Client
-
-	resp, err := client.Servers.Delete(context.Background(), d.Id())
+// upgradePlan is a function to upgrade and commit a server to the next plan.
+func upgradePlan(conf *helper.CombinedConfig, client *server.Client, d *schema.ResourceData) diag.Diagnostics {
+	res, err := client.Upgrade(context.Background(), server.UpgradeRequest{
+		Name: d.Id(),
+		Plan: fmt.Sprint(d.Get("product_code")),
+	})
 	if err != nil {
-		return diag.Errorf("Error deleting server: %s", err)
+		return diag.Errorf("Error upgrading server: %s", err)
 	}
 
-	if err = helper.WaitForAction(client, resp.Return.JobID); err != nil {
+	if !res.Status {
+		return diag.Errorf("Error upgrading server: %s", res.Msg)
+	}
+
+	resp, err := client.CommitDiskChanges(context.Background(), server.CommitDiskChangesRequest{
+		ServerName: d.Id(),
+	})
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if !res.Status {
+		return diag.Errorf("Error upgrading server: %s", res.Msg)
+	}
+
+	if err := helper.WaitForAction(conf.Client, resp.Return.JobID); err != nil {
 		return diag.FromErr(err)
 	}
 
 	return nil
 }
 
-// setServerAttributes is a function to set data to a Server resource.
-func setServerAttributes(d *schema.ResourceData, server *gosh.Server) error {
+// updateLabel is a function to update a label of a server.
+func updateLabel(client *server.Client, d *schema.ResourceData) diag.Diagnostics {
+	res, err := client.Update(context.Background(), server.UpdateRequest{
+		Name:  d.Id(),
+		Label: fmt.Sprint(d.Get("label")),
+	})
+	if err != nil {
+		return diag.Errorf("Error updating server: %s", err)
+	}
+
+	if !res.Status {
+		return diag.Errorf("Error updating server: %s", res.Msg)
+	}
+
+	return nil
+}
+
+// deleteResource is a function to delete a server.
+func deleteResource(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	conf, ok := meta.(*helper.CombinedConfig)
+	if !ok {
+		return diag.Errorf("failed to convert meta object")
+	}
+
+	client := server.New(conf.Client)
+
+	resp, err := client.Delete(context.Background(), server.DeleteRequest{
+		Name: d.Id(),
+	})
+	if err != nil {
+		return diag.Errorf("Error deleting server: %s", err)
+	}
+
+	if !resp.Status {
+		return diag.Errorf("Error deleting server: %s", resp.Msg)
+	}
+
+	if err := helper.WaitForAction(conf.Client, resp.Return.JobID); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+// setServerAttributes is a function to set data to a server.
+func setServerAttributes(d *schema.ResourceData, server models.Server) error {
 	return d.Set("name", server.Name)
 }
