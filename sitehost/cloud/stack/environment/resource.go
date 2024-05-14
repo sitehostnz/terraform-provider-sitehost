1a0533a5a1dd2b215e57aa9b4bd91d6c191beb9e
John Lennard <john@yakmoo.se>
2024-05-14 12:33:32 +1200

    add in some fixes for environment updates

--- sitehost/cloud/stack/environment/resource.go
+++ sitehost/cloud/stack/environment/resource.go
@@ -84,15 +84,20 @@ func updateResource(ctx context.Context, d *schema.ResourceData, meta interface{
 	if d.HasChange("settings") {
 		old, new := d.GetChange("settings")
 
-		// additions
+		// things that exist in new, need to be added or updated.
 		for k, v := range new.(map[string]interface{}) {
 			ev := models.EnvironmentVariable{Name: k, Content: fmt.Sprintf("%v", v)}
-			if _, exists := old.(map[string]interface{})[k]; !exists {
+			//if _, exists := old.(map[string]interface{})[k]; !exists {
+			//	environmentVariables = append(environmentVariables, ev)
+			//}
+
+			// if the content is different or does not exist, then we need to update it.
+			if old.(map[string]interface{})[k] != new.(map[string]interface{})[k] {
 				environmentVariables = append(environmentVariables, ev)
 			}
 		}
 
-		// removals
+		// removals - if something does not exist in the new map, then we need to remove it.
 		for k, _ := range old.(map[string]interface{}) {
 			ev := models.EnvironmentVariable{Name: k, Content: ""}
 			if _, exists := new.(map[string]interface{})[k]; !exists {
@@ -101,27 +106,29 @@ func updateResource(ctx context.Context, d *schema.ResourceData, meta interface{
 		}
 	}
 
-	client := environment.New(conf.Client)
-	response, err := client.Update(
-		ctx,
-		environment.UpdateRequest{
-			ServerName:           serverName,
-			Project:              project,
-			Service:              service,
-			EnvironmentVariables: environmentVariables,
-		})
+	// if we have changes... then we need to push em...
+	if len(environmentVariables) > 0 {
+		client := environment.New(conf.Client)
+		response, err := client.Update(
+			ctx,
+			environment.UpdateRequest{
+				ServerName:           serverName,
+				Project:              project,
+				Service:              service,
+				EnvironmentVariables: environmentVariables,
+			})
 
-	if nil != err {
-		return diag.Errorf("Error updating environment info: %s", err)
+		if nil != err {
+			return diag.Errorf("Error updating environment info: %s", err)
+		}
+
+		if err := helper.WaitForAction(conf.Client, job.GetRequest{JobID: response.Return.JobID, Type: job.SchedulerType}); err != nil {
+			return diag.FromErr(err)
+		}
+
 	}
 
 	d.SetId(fmt.Sprintf("%s/%s/%s", serverName, project, service))
-
-	if err := helper.WaitForAction(conf.Client, job.GetRequest{JobID: response.Return.JobID, Type: job.SchedulerType}); err != nil {
-
-		return diag.FromErr(err)
-	}
-
 	return nil
 }
 
