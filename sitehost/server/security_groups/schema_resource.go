04af20fddbb0a0f9d519ed1926120db77c0d3b31
Lucas RiedlShah <lucas.riedlshah@sitehost.nz>
2025-06-04 09:24:44 +1200

    Flesh out resource schema for Security Group resource

--- sitehost/server/security_groups/schema_resource.go
+++ sitehost/server/security_groups/schema_resource.go
@@ -5,18 +5,18 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
 
-// resourceSchema is the schema with values for a Server resource.
+// resourceSchema is the schema with values for a Security Group resource.
 var resourceSchema = map[string]*schema.Schema{
 	"name": {
 		Type:        schema.TypeString,
-		Optional:    true,
+		Required:    true,
 		Computed:    true,
 		ForceNew:    true,
 		Description: "The name of the Security Group.",
 	},
 	"label": {
 		Type:        schema.TypeString,
-		Required:    true,
+		Optional:    true,
 		Description: "The label for the Security Group.",
 	},
 	"rules": {
@@ -27,37 +27,37 @@ var resourceSchema = map[string]*schema.Schema{
 				Elem: map[string]*schema.Schema{
 					"enabled": {
 						Type:        schema.TypeBool,
-						Required:    true,
+						Optional:    true,
 						Description: "Whether this inbound rule is enabled or not.",
 					},
 					"action": {
 						Type:     schema.TypeString,
-						Required: true,
+						Optional: true,
 						ValidateFunc: validation.StringInSlice([]string{
 							"ACCEPT",
 							"DROP",
 							"REJECT",
 						}, false),
-						Description: "Whether this inbound rule is enabled or not.",
+						Description: "The action for this inbound rule. The following values are accepted: ACCEPT, DROP, REJECT.",
 					},
 					"protocol": {
 						Type:     schema.TypeString,
-						Required: true,
+						Optional: true,
 						ValidateFunc: validation.StringInSlice([]string{
 							"tcp",
 							"udp",
 						}, false),
-						Description: "Whether this inbound rule is enabled or not.",
+						Description: "The protocol for this inbound rule. The following values are accepted: tcp, udp.",
 					},
 					"src_ip": {
 						Type:        schema.TypeString,
-						Required:    true,
-						Description: "Whether this inbound rule is enabled or not.",
+						Optional:    true,
+						Description: "The source IP address for this inbound rule. This can either be a standalone IP or CIDR range.",
 					},
 					"dest_port": {
 						Type:        schema.TypeString,
-						Required:    true,
-						Description: "Whether this inbound rule is enabled or not.",
+						Optional:    true,
+						Description: "The destination port for this inbound rule.",
 					},
 				},
 			},
@@ -66,42 +66,42 @@ var resourceSchema = map[string]*schema.Schema{
 				Elem: map[string]*schema.Schema{
 					"enabled": {
 						Type:        schema.TypeBool,
-						Required:    true,
+						Optional:    true,
 						Description: "Whether this inbound rule is enabled or not.",
 					},
 					"action": {
 						Type:     schema.TypeString,
-						Required: true,
+						Optional: true,
 						ValidateFunc: validation.StringInSlice([]string{
 							"ACCEPT",
 							"DROP",
 							"REJECT",
 						}, false),
-						Description: "Whether this inbound rule is enabled or not.",
+						Description: "The action for this inbound rule. The following values are accepted: ACCEPT, DROP, REJECT.",
 					},
 					"protocol": {
 						Type:     schema.TypeString,
-						Required: true,
+						Optional: true,
 						ValidateFunc: validation.StringInSlice([]string{
 							"tcp",
 							"udp",
 						}, false),
-						Description: "Whether this inbound rule is enabled or not.",
+						Description: "The protocol for this inbound rule. The following values are accepted: tcp, udp.",
 					},
 					"dest_ip": {
 						Type:        schema.TypeString,
-						Required:    true,
-						Description: "Whether this inbound rule is enabled or not.",
+						Optional:    true,
+						Description: "The destination IP address for this outbound rule. This can either be a standalone IP or CIDR range.",
 					},
 					"dest_port": {
 						Type:        schema.TypeString,
-						Required:    true,
-						Description: "Whether this inbound rule is enabled or not.",
+						Optional:    true,
+						Description: "The destination port for this inbound rule.",
 					},
 				},
 			},
 		},
-		Optional:    true,
-		Description: "The security groups which this server uses.",
+		Required:    true,
+		Description: "The rules which the security group follows.",
 	},
 }
